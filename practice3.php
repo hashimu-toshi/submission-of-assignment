3c01c04b51a697595c61bd5607634f3f246f3818
hashimura <hashimu.toshi@gmail.com>
2021-08-10 13:15:37 +0000

    課題03の再提出

--- practice3.php
+++ practice3.php
@@ -1,37 +1,34 @@
 <?php
-    $name = "Toshihiro";
-    if ($name = "Toshihiro") {
-        echo "私はとしひろです。";
-    } else{
-        echo "としひろではありません。";
-    }
- ?>
- 
- <?php
-    $total = 0;
-    
-    for ($i = 0 ; $i <= 10000 ; $i++ ) {
-        $total += $i;
-    }
-    echo $total;
- ?>
- 
- <?php
-    $fruits = array ("peach" , "cherry" , "pear" , "water-melon" , "banana");
-    foreach ($fruits as $fruit) {
-        echo $fruit;
-        echo "\n";
-    }
- ?>
- 
-<?php
+$name = "Toshihiro";
+if ($name == "Toshihiro") {
+    echo "私はToshihiroです。";
+} else{
+    echo "Toshihiroではありません。";
+}
+
+echo "\n";
+
+$total = 0;
+
+for ($i = 1 ; $i <= 10000 ; $i++ ) {
+    $total += $i;
+}
+echo $total;
+
+echo "\n";
+
+$fruits = array ("peach" , "cherry" , "pear" , "water-melon" , "banana");
+foreach ($fruits as $fruit) {
+    echo $fruit;
+    echo "\n";
+}
+
 $start = 1;
 $end = 100;
-
 for($i = $start; $i <= $end; $i++){
 
-  if(($i % 5) == 0){
+if(($i % 5) == 0){
     echo $i;
+    echo "\n";
   }
 }
-?>
